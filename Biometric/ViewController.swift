865f8135d2365b20ec662afab216b7f7d00e0f40
batikansosun <batikansosun@gmail.com>
2021-02-15 00:48:34 +0300

    init

--- Biometric/ViewController.swift
+++ Biometric/ViewController.swift
@@ -6,12 +6,42 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        userBiometricAuthentication()
+    }
+    
+    
+    func userBiometricAuthentication() {
+        let localAuthenticationContext = LAContext()
+        var authorizationError: NSError?
+        let reason = "Authentication required to login"
+
+        if localAuthenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authorizationError) {
+            localAuthenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, evaluateError in
+                if success {
+                    print("Authenticated succesfully!")
+                    
+                } else {
+                    // Failed to authenticate
+                    guard let error = evaluateError else {
+                        return
+                    }
+                    print(error)
+                }
+            }
+        } else {
+            guard let error = authorizationError else {
+                return
+            }
+            print(error)
+        }
     }
 
 
